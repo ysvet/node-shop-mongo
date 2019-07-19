b74624929387258c2a2e312a8327e08e0039994a
Yuri Svet <iurie.shvets@gmail.com>
2019-07-19 12:12:57 +0300

    first commit

---
+++ routes(vanilla_node_js).js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type ="text" name="message1"></input><button type="submit">SEND</button></form> </body>');
+        res.write('</html>');
+        return res.end();
+    }
+    
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
+    
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first node app</title></head>');
+    res.write('<body><h1>HELLO FROM MY NODE.JS SERVER!</h1></body>');
+    res.write('</html>');
+    res.end();
+}
+
+module.exports = {
+   handler:  requestHandler,
+   someText: "some hardcoded text" 
+};
