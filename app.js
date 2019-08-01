6adfc089d3b03d312cee8fca84eafa5314c40cf9
Yuri Svet <iurie.shvets@gmail.com>
2019-08-01 13:32:33 +0300

    add to cart functionality restored

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('5d40389a1c9d440000d767fb')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -16,10 +16,26 @@ class User {
   }
 
   addToCart(product) {
-    // const cartProduct = this.cart.items.fndIndex( (cp) => {
-    //   return cp._id === product._id;
-    // });
-    const updatedCart = { items: [{...product, quantity: 1}]};
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+        updatedCartItems.push({
+          productId: new ObjectId(product._id), 
+          quantity: newQuantity
+        });
+    }
+   
+
+    const updatedCart = { 
+      items: updatedCartItems
+    };
     const db = getDb();
     return db
       .collection('users')
@@ -27,6 +43,27 @@ class User {
         {_id: new ObjectId(this._id)},
         {$set: {cart: updatedCart}}
       );
+  }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map(i => {
+      return i.productId;
+    });
+    return db
+      .collection('products')
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then(products => {
+        return products.map(p => {
+          return {
+            ...p,
+            quantity: this.cart.items.find(i => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity
+          };
+        });
+      });
   }
 
   static findById(userId) {
